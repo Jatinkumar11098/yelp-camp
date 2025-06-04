45f09913c1c1aae5a73e858c87e4daa7da83d7ff
Jatinkumar11098 <jxeffects@gmail.com>
2025-06-04 18:22:14 -0400

    add show route and home.ejs

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 
+// mongoose connection
+main().catch(err => console.log(err));
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+    console.log('Mongoose connection successfull!!')
+}
+
+
+
+// routes 
+// show route 
+app.get('/campgrounds', (req, res) => {
+    res.send('This will be the show route!!');
+})
+
 app.listen(3000, () => {
     console.log('listening to port 3000!!');
 });
