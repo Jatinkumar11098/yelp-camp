1f850fce7d14a5a95df0c2239d7be480190dfc6c
Jatinkumar11098 <jxeffects@gmail.com>
2025-06-06 14:51:45 -0700

    minor fix seedDB

--- seedfiles/seedDB.js
+++ seedfiles/seedDB.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Campground = require('../models/campground.js')
+const campground = require('../models/campground.js')
 const cities = require('./cities.js')
 const { descriptors, places } = require('./seedHelpers.js')
 
@@ -8,21 +8,21 @@ const { descriptors, places } = require('./seedHelpers.js')
 main().catch(err => console.log(err));
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
-    console.log('Mongoose connection successfull!!')
+    console.log('Mongoose connection and seeding successful!')
 }
 
 
 // function for random array values 
 const arrayRand = array => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
-    await Campground.deleteMany();
+    await campground.deleteMany();
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
-        const Camp = new Campground({
+        const camp = new campground({
             location: `${cities[random1000].city} ${cities[random1000].state}`,
             title: `${arrayRand(descriptors)} ${arrayRand(places)}`
         })
-        await Camp.save()
+        await camp.save()
     }
 }
 
