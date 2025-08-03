72bd058b49cefeefe95c1ea00bc67ab8bc389887
Jatinkumar11098 <jxeffects@gmail.com>
2025-08-03 19:53:01 -0400

    add router to reviews

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const {reviewSchema } = require('./schemas.js');
 const ejsMate = require('ejs-mate');
 const Joi = require('joi');
 const ExpressError = require('./utils/expressError');
-const router = require('./routes/campgrounds.js');
+const campgrounds = require('./routes/campgrounds.js');
+const reviews = require('./routes/reviews.js');
 
 
 app.engine('ejs', ejsMate);
@@ -21,7 +22,9 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-app.use('/campgrounds', router);
+app.use('/campgrounds', campgrounds);
+app.use('/campgrounds/:id/reviews', reviews);
+
 // mongoose connection
 main().catch(err => console.log(err));
 async function main() {
@@ -29,34 +32,6 @@ async function main() {
     console.log('Mongoose connection successfull!!')
 }
 
-// middlewares
-
-const validateReview = (req, res, next) => {
-    const {error}= reviewSchema.validate(req.body); 
-    if (error) {
-        const message = error.details.map(mes => mes.message).join(',');
-        throw new ExpressError(message, 400);
-    }
-    else next();
-}
-
-// review create
-app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
-    const foundCamp = await campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    foundCamp.reviews.push(review);
-    await review.save();
-    await foundCamp.save();
-    res.redirect(`/campgrounds/${foundCamp._id}`);
-}))
-// review delete 
-app.delete('/campgrounds/:id/reviews/:reviewID', catchAsync(async(req,res)=>{
-    const {id, reviewID} = req.params;
-    await campground.findByIdAndUpdate(id, {$pull:{reviews:reviewID}});
-    Review.findByIdAndDelete(reviewID);
-    res.redirect(`/campgrounds/${id}`);
-
-}))
 
 app.all(/(.*)/, (req, res, next) => {
     next(new expressError('Page not found', 404));
@@ -72,9 +47,3 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
     console.log('listening to port 3000!!');
 });
-
-
-
-
-
-// copy paste 
---
+++ routes/reviews.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router({mergeParams:true});
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/expressError');
+const campground = require('../models/campground');
+const {reviewSchema } = require('../schemas.js');
+const Review = require('../models/review.js');
+
+
+
+// middlewares
+
+const validateReview = (req, res, next) => {
+    const {error}= reviewSchema.validate(req.body); 
+    if (error) {
+        const message = error.details.map(mes => mes.message).join(',');
+        throw new ExpressError(message, 400);
+    }
+    else next();
+}
+
+// review create
+router.post('/', validateReview, catchAsync(async (req, res) => {
+    const foundCamp = await campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    foundCamp.reviews.push(review);
+    await review.save();
+    await foundCamp.save();
+    res.redirect(`/campgrounds/${foundCamp._id}`);
+}))
+// review delete 
+router.delete('/:reviewID', catchAsync(async(req,res)=>{
+    const {id, reviewID} = req.params;
+    await campground.findByIdAndUpdate(id, {$pull:{reviews:reviewID}});
+    Review.findByIdAndDelete(reviewID);
+    res.redirect(`/campgrounds/${id}`);
+
+}))
+
+
+module.exports = router;
