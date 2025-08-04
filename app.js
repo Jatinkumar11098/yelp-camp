103a2fd1f43d3b86add26ee442a0b77e5fe8c5ad
Jatinkumar11098 <jxeffects@gmail.com>
2025-08-04 04:59:39 -0400

    add static assets

--- app.js
+++ app.js
@@ -16,10 +16,11 @@ const reviews = require('./routes/reviews.js');
 
 
 app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(methodOverride('_method'));
 
 app.use('/campgrounds', campgrounds);
