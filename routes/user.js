1dd8e15a2d8be4e3e356c854e902543fb570ebfa
Jatinkumar11098 <jxeffects@gmail.com>
2025-08-19 16:37:09 -0400

    add routes for register

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
 router.get('/register', (req, res) => {
     res.render('./users/register');
 });
 
-
+router.post('/register', async(req,res)=>{
+    const {username, email, password} =req.body;
+    const user = new User({username,email});
+    const registerUser = await User.register(user, password);
+    res.send(registerUser);
+});
 
 
 module.exports = router;
