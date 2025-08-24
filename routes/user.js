e3c16e2343a4d7c38e834ee279cc53ab26b00e4f
Jatinkumar11098 <jxeffects@gmail.com>
2025-08-24 11:33:10 -0400

    add error handling on register

--- routes/user.js
+++ routes/user.js
@@ -3,22 +3,29 @@ const router = express.Router();
 const User = require('../models/user');
 const user = require('../models/user');
 const passport = require('passport');
+const catchAsync = require('../utils/catchAsync');
 
 router.get('/register', (req, res) => {
     res.render('./users/register');
 });
 
-router.post('/register', async (req, res) => {
-    const { username, email, password } = req.body;
-    const user = new User({ username, email });
-    const registerUser = await User.register(user, password);
-    await registerUser.save();
-    req.login(registerUser, (err) => {
-        if (err) return next(err);
-        req.flash('success', 'Welcome to Yelpcamp');
-        res.redirect('/campgrounds');
-    })
-});
+router.post('/register', catchAsync(async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const user = new User({ username, email });
+        const registerUser = await User.register(user, password);
+        await registerUser.save();
+        req.login(registerUser, (err) => {
+            if (err) return next(err);
+            req.flash('success', 'Welcome to Yelpcamp');
+            res.redirect('/campgrounds');
+        })
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+
+}));
 
 // login routes 
 router.get('/login', (req, res) => {
