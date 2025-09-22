5d92ed146e7ab4dbdd9f0a7207b042404eb9dc9e
Jatinkumar11098 <jxeffects@gmail.com>
2025-09-22 17:36:17 -0400

    fix map on show page

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,8 +1,18 @@
 maptilersdk.config.apiKey = mapAPI;
 
-        const map = new maptilersdk.Map({
-            container: 'map', // container's id or the HTML element in which the SDK will render the map
-            style: maptilersdk.MapStyle.STREETS,
-            center: campground.geometry.coordinates, // starting position [lng, lat]
-            zoom: 14 // starting zoom
-        });
+const map = new maptilersdk.Map({
+    container: 'map', // container's id or the HTML element in which the SDK will render the map
+    style: maptilersdk.MapStyle.STREETS,
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 14 // starting zoom
+});
+
+new maptilersdk.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new maptilersdk.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
+            )
+    )
+    .addTo(map)
