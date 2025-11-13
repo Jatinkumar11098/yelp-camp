0d68327fafa22153b3cc05ecd576346d9682c432
Jatinkumar11098 <jxeffects@gmail.com>
2025-11-13 12:52:44 -0500

    fix cluster map settings

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -31,19 +31,19 @@ map.on('load', function () {
             'circle-color': [
                 'step',
                 ['get', 'point_count'],
-                '#51bbd6',
-                100,
-                '#f1f075',
-                750,
-                '#f28cb1'
+                '#caf0f8',
+                6,
+                '#00b4d8',
+                12,
+                '#0096c7'
             ],
             'circle-radius': [
                 'step',
                 ['get', 'point_count'],
                 20,
-                100,
+                6,
                 30,
-                750,
+                12,
                 40
             ]
         }
